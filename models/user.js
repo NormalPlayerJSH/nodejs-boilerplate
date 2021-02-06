b22e148dba6e08c5bdbe6308b79e96dccb170ce1
NormalPlayerJSH <jsh010904@gmail.com>
2021-02-07 03:27:55 +0900

    git Test

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        maxlength: 50
+    },
+    email:{
+        type:String,
+        trim:true, // 문자열 사이의 공백을 없앰
+        unique: 1  // 중복되는 이메일 사용 금지
+    },
+    password:{
+        type:String,
+        minlength:5
+    },
+    lastname:{
+        type:String,
+        maxlength:50
+    },
+    role:{
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token:{
+        type: String
+    },
+    tokenExp:{
+        type: Number
+    }
+})
+
+const User=mongoose.model('User',userSchema)
+
+module.export={User}
