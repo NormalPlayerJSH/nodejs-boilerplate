fa3e5892e8a39084449988a93349b4c10a0b1602
NormalPlayerJSH <jsh010904@gmail.com>
2021-02-08 03:09:18 +0900

    URI 보호 처리 등

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser=require('body-parser')
+const {User}=require("./models/User")
+
+const config=require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+
+// application/json
+app.use(bodyParser.json())
+
 const mongoose=require('mongoose')
-mongoose.connect('mongodb+srv://sehwanjeon:sehwanjeon@boilerplate.357pv.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -12,7 +24,22 @@ mongoose.connect('mongodb+srv://sehwanjeon:sehwanjeon@boilerplate.357pv.mongodb.
   .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello, World!')
+})
+
+app.post('/register',(req,res)=>{
+  // 회원가입 시 필요한 정보들을 클라이언트에서 가져오면
+  // 이들을 데이터베이스에 저장한다
+
+  const user=new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
 })
 
 app.listen(port, () => {
