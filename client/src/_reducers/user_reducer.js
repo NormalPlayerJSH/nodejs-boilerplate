170dbb9629d0a070fda621479c2016294642593e
NormalPlayerJSH <jsh010904@gmail.com>
2021-04-05 19:05:09 +0900

    끝

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,5 +1,7 @@
 import {
-    LOGIN_USER
+    LOGIN_USER,
+    REGISTER_USER,
+    AUTH_USER
 } from '../_actions/types'
 
 
@@ -8,6 +10,14 @@ export default function (state={}, action){
     switch (action.type) {
         case LOGIN_USER:
             return {... state,loginSuccess: action.payload}
+            break;
+
+        case REGISTER_USER:
+            return {...state, register:action.payload}
+            break;
+
+        case AUTH_USER:
+            return {...state, userData:action.payload}
             break;
     
         default:
